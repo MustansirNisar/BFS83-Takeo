bf5a8bf78f7d0c4f1994f01f8a46b0940a4c0353
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:24:50 +0530

    Add files via upload

---
+++ JavaScript/ArithmeticOps.js
@@ -0,0 +1,47 @@
+// let a = 20;
+// let b = 2;
+// let sum = a+b;
+// console.log(sum)
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a * b);
+// console.log(a / b); 
+// console.log(a % b);
+// console.log(a ** b);
+
+// let x = 5;
+// console.log(x-1)
+// x++;
+// ++x;
+// x--;
+// --x;
+// console.log(x)
+// console.log(x++)
+// console.log(++x)
+// console.log(x--)
+// console.log(--x)
+
+
+// let a = 6;
+// a++;
+// ++a;
+// console.log(a)
+// console.log(a++) prints 6, then a becomes 7 
+// console.log(++a) a becomes 7, then prints 7
+// console.log(a)
+// console.log(a++)
+// console.log(++a)
+// console.log(a)
+// console.log(a--)
+// console.log(--a)
+// console.log(a)
+
+//Unary Plus Operator to convert an operand to a Number
+// let a = "23"
+// console.log(typeof a)
+// let num = +a;
+// console.log(typeof num)
+// console.log(+true)
+// console.log(+false)
+// console.log(+null)
+// console.log(+"abdc")
