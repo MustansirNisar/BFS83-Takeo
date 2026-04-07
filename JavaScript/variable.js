8735f2a38f60b6322aafb9cd058ef48635007c59
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:23:23 +0530

    Add files via upload

---
+++ JavaScript/variable.js
@@ -0,0 +1,50 @@
+// var age; //Declaration
+// age = 20; //Initialization
+
+// var age = 10; //Declaration + Initialization
+
+// let marks; //Declaration
+// marks = 90; //Initialization
+// let marks = 90; //Declaration + Initialization
+
+// const age = 10; // Declaration + Initialization
+
+
+// var age;
+// age=30;
+// // console.log(age)
+// var age;
+// age = 40;
+// age = 10;
+// console.log(age)
+
+// let marks;
+// marks = 100;
+// console.log(marks)
+// // let marks;
+// marks = 50;
+// console.log(marks)
+
+// const age = 10;
+// // const age = 30;
+// age = 20;
+// console.log(age)
+
+
+
+// let name; //valid
+// let age4; //valid
+// let _temp; //valid
+// let $cost; //valid
+
+
+// let 1name;
+
+// let Name = "Diksha"
+// let name = "Sandeep"
+
+// console.log(Name)
+// console.log(name)
+
+// let var = 10; //invalid
+// let function = 30; //invalid
