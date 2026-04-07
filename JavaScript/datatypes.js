9fd1321a55f171be6117ed80d6cf6a3ea4b7b079
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:23:45 +0530

    Add files via upload

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,43 @@
+//string
+// let fName = "Nitish";
+// console.log(typeof fName)
+// let age = "30";
+// console.log(typeof age)
+
+//Number
+// let age = 30;
+// console.log(typeof age)
+// let price = 300.40;
+// console.log(typeof price)
+
+//boolean
+// let isLoggedIn = true;
+// let isLoggedIn = false;
+// console.log(typeof isLoggedIn)
+
+//Undefined
+// let marks; //can be assigned a value later on
+// console.log(typeof marks)
+
+//null
+// let user = null; //intentional absence of value
+// console.log(user)
+// console.log(typeof user)
+
+//bigint
+// let bNumber = 12345678765432234567876543234567n;
+// console.log(bNumber)
+// console.log(typeof bNumber)
+// let num = BigInt("1234564783903948767389948767389")
+// console.log(num)
+
+// symbol
+// let id = Symbol("id")
+// let idd = Symbol("id")
+// // console.log(typeof id)
+// console.log(id==idd)
+
+
+
+
+// let price = true;
