96d240446cc96fb648acec3401c49c816d8eaaad
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:24:17 +0530

    Add files via upload

---
+++ JavaScript/comparisonOps.js
@@ -0,0 +1,35 @@
+// let a = 10;
+// let b = 20;
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+// console.log(a != b)
+// console.log(a == b)
+
+//Strict Vs Loose Comparison
+// let a = 20;
+// let b = "20";
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a!=b)
+// console.log(a!==b)
+
+// let a = 10;
+// let b = "5";
+// let c = "abc"
+// // console.log(a > b)
+// // console.log(a < b)
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a / b)
+// console.log(a + c)
+// console.log(a - c)
+// console.log(a * b)
+
+// let a = "234"
+// let b = Number(a)
+// console.log( typeof b)
+
+// console.log(Boolean(1))
