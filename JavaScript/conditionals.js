c2d52413a24e6b9c5ad87107fbfbb6285bced4c4
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:25:06 +0530

    Add files via upload

---
+++ JavaScript/conditionals.js
@@ -0,0 +1,81 @@
+// if statement
+// if (condition){
+//     //code to excecute if condition is true
+// }
+
+// let age = 30;
+// if(age>40){
+//     console.log("You are an adult")
+//     console.log("Hello and welcome")
+// }
+// if(age<35){
+//     console.log("Welcome to the club")
+// }
+
+// console.log("How are you doing?")
+
+// if....else statement
+// let age = 39;
+// if(age>40){
+//     console.log("You are in your fourties")
+// }
+// else{
+//     console.log("you are a child")
+// }
+// let marks = 30;
+// if(marks>40){
+//     console.log("Qualified")
+// }
+// else{
+//     console.log("Not qualified")
+// }
+
+
+// if...else if...else Ladder
+// let marks = 80;
+// if(marks>=90){
+//     console.log("Grade: A");
+// }
+// else if(marks>=80){
+//     console.log("Grade: B")
+// }
+// else if(marks>=70){
+//     console.log("Grade: C")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+
+//Ternary Operator
+// condition?value_if_true:value_if_flase
+// let age = 19;
+// age>18?console.log("You are an adult"):console.log("Not an adult")
+
+// let marks = 30;
+//  marks>=90?console.log("Grade:A")
+// :marks>=75?console.log("Grade:B")
+// :marks>=60?console.log("Grade:C")
+// :marks>=40?console.log("Grade:D")
+// :console.log("Fail")
+
+//Switch statement
+// let day = "Monday";
+// switch (day){
+//     case "Monday":
+//         console.log("It is the start of the week");
+//         break;
+//     case "Friday":
+//         console.log("Weekend is coming");
+//         break;
+//     case "Saturday":
+//         console.log("Weekend is here")
+//         break;
+//     default:
+//         console.log("Some other day")
+// }
+
+//prompt()
+// let age = Number(prompt("Enter your age: "))
+// console.log("Your age is: ", age)
+// console.log(typeof age)
