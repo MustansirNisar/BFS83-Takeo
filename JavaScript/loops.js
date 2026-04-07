ecce97a1809aa9fc2f4054ba42a1431f54862c06
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:22:58 +0530

    Add files via upload

---
+++ JavaScript/loops.js
@@ -0,0 +1,52 @@
+//for loop...you know in advance how many times to loop
+// for(initialization; condition; increment/updation){
+//     //code to run
+// }
+
+// for(let i=0; i<10; i++){
+//     console.log("Hello and Welcome", i)   
+// }
+
+// for(let i=10; i>=0; i--){
+//     console.log("Hello and welcome",i)
+// }
+
+//Calculate the sum of first ten natural numbers
+// let sum = 0;
+// for(let i=1; i<=10; i++){
+//     sum += i;
+// }
+// console.log("The sum of the first ten natural numbers is: ", sum)
+
+
+
+//while loop....when the number of iterations is not known
+// while(condition){
+//     //code to execute
+// }
+
+// let i=0;  //initialization
+// while(i<10){ //condition
+//     console.log("Hello and Welcome",i)
+//     i++; //updation
+// }
+
+//example
+// let password = "";
+// while(password !== "secure12"){
+//     password = prompt("Enter the correct password: ");
+// }
+
+// console.log("Logged in")
+
+
+//do..while loop
+// do{
+//     //code to execute
+// }while(condition);
+
+let i = 11; //initialization
+do{
+    console.log("Hello and Welcome",i)
+    i++; //updation
+}while(i<10); //condition
