8f436fb221f7ea003d93cad0f742657dec37aea5
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-07 19:24:01 +0530

    Add files via upload

---
+++ JavaScript/assignmentOps.js
@@ -0,0 +1,21 @@
+// let age = 30;
+
+// let marks = 90;
+// marks = marks + 10;
+// marks += 10
+// marks = marks - 20;
+// marks -= 20;
+// marks = marks / 30;
+// marks /= 30
+// console.log(marks)
+
+let a = 10;
+// a += 5;
+// a -= 10;
+// a *= 5;
+// a /= 5;
+// a %= 4;
+// a **= 4;
+
+
+console.log(a)
