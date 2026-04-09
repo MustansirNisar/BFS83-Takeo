ebc717468604930c5af82a016607e382dd03753c
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-09 08:55:14 +0530

    Add files via upload

---
+++ JavaScript/scope.js
@@ -0,0 +1,44 @@
+// let fName = "Sushant"
+// const fName = "Sushant"
+// var fName = "Sushant"
+
+// function greet(){
+//     console.log("Hello", fName)
+// }
+
+// greet();
+
+
+// function greet(){
+//     const fName = "Sushant"
+//     console.log("Hello", fName)
+// }
+
+// greet();
+// console.log(fName)
+
+// if(true){
+//     var fName = "Sandeep"
+//     console.log("Hello", fName)
+// }
+
+// if(true){
+//     let fName = "Sandeep"
+//     console.log("Hello", fName)
+// }
+
+// if(true){
+//     const fName = "Sandeep"
+//     console.log("Hello", fName)
+// }
+// console.log(fName)
+
+{
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+console.log(c)
+console.log(b)
+console.log(a)
