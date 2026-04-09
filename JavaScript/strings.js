07bd81fb5b697d1e59dbb80c4b68c2e7d4566687
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-09 08:55:33 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,63 @@
+// let fName = "Nitish"
+// let userName = "Sandeep"
+// let name = 'Diksha'
+// console.log(fName, userName, name)
+
+// let userName = "Sandeep"
+// // console.log(userName.length)
+// // console.log(userName[3])
+// // console.log(userName[5])
+// console.log(userName[0])
+
+//toUpperCase/toLowerCase....to convert the string to upper or lower case
+// let fName = "Sushant"
+// let newString = fName.toUpperCase();
+// // console.log(fName.toUpperCase())
+// // console.log(fName.toLowerCase())
+// console.log(newString)
+// console.log(fName)
+
+//trim()....remove the whitespaces from both sides
+// let fName = " Nitish "
+// console.log(fName.length)
+// console.log(fName)
+// console.log(fName.trim())
+
+//slice()...extract a portion of the string
+// let fName = "Nitish"
+// console.log(fName.slice(0,4))
+// console.log(fName.slice(-4,-1))
+// console.log(fName.slice(3))
+
+//substring()
+
+//concat()...Joins two or more string together
+// let str1 = "Hello"
+// let str2 = "Diksha"
+// console.log(str1.concat(" ", str2, str3, str4...))
+// console.log(str1 + " " + str2)
+
+//replace()....returns a new string with the first occurrence of a substring by a new value
+//replcaeAll()...to replace all the occrrences of a value.
+// let text = "I like football as football is a very interesting sport."
+// console.log(text.replace("football", "Cricket"))
+// console.log(text.replaceAll("football", "Cricket"))
+
+
+
+//Template Literals
+// let fName = "Nitish"
+// console.log("Hello and Welcome", fName)
+// console.log(`Hello and Welcome ${fName}`)
+
+// let a = 10;
+// let b = 20;
+// console.log(`The sum of ${a} and ${b} is: ${a+b}`)
+
+
+// let poem = "Twinkle Twinkle little star \n How I wonder what you are"
+// let poem = `Twinkle Twinkle little star
+//  How I wonder what you are
+//  Up above the world so high`
+
+// console.log(poem)
