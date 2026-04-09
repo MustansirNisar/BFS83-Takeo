2f0a4d253c9235753785c6946c39d84a0a0cb388
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-09 08:55:56 +0530

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,124 @@
+// function functionName(){
+//     //code to exectue   //Function Definition/Function Declaration
+// }
+
+// function greet(){
+//     console.log("Hello and welcome")
+//     console.log("How are you doing?")
+// }
+
+
+// //Function call/Invocation
+// greet();
+
+// function greet(){
+//     for(let i=0;i<=10;i++){
+//     console.log("Hello and Welcome")
+// }
+// }
+
+// greet();
+// console.log("line")
+// console.log("line")
+// console.log("line")
+// console.log("line")
+// console.log("line")
+// greet();
+
+
+// Function with parameters
+// function greet(firstName){
+//     console.log("Hello and Welcome ", firstName);
+// }
+
+
+// greet("diksha");
+// greet("Nitish");
+// greet("Sushant");
+
+// function addition(a,b){
+//     let sum = a+b
+//     console.log("Sum:", sum)
+//     console.log(typeof sum)
+//     // console.log("The sum of the numbers is :", a+b)
+
+// }
+
+// addition(10, 4)
+// addition(20, 10)
+// addition(2, 10)
+
+//Functions with return statement
+// function add(a,b){
+//     return a+b;
+//     // console.log(a+b)
+// }
+
+// let result = add(4,5)
+// console.log(result)
+
+// console.log(add(4,5))
+
+// function number(n){
+//     if(n>0) return "Positive";
+//     if(n<0) return "Negative";
+//     return "Zero";
+// }
+
+
+// console.log(number(0))
+
+
+
+// function num(n){
+//     console.log("Before the return statement");
+//     return n*2
+//     console.log("After the return statement")
+// }
+
+// console.log(num(4))
+
+//Anonymous functions
+// let greet = function (fName){
+//     console.log("How are you?", fName)
+// }
+
+// greet("Nitish");
+
+//Arrow Functions
+// let greet = ()=>{
+//     console.log("How are you ?")
+// }
+
+// greet();
+
+// let square = a => a**2
+// console.log(square(4))
+
+
+//IIFE
+
+// (function greet(){
+//     console.log("Hello and Welcome")
+// })();
+
+
+// var fName = "Diksha"
+// var fName = "Nitish"
+// console.log(fName)
+
+// (
+//     function(){
+//         var fName = "Diksha"
+//         console.log("Dev 1:", fName)
+//     }
+// )();
+
+// (
+//     function(){
+//         var fName = "Nitish"
+//         console.log("Dev 2:", fName)
+//     }
+// )();
+
+// console.log(fName)
