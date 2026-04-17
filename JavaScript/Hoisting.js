8a4c21fb470b15df22ca9f70c76af8fe1d700525
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-04-17 09:22:33 +0530

    Add files via upload

---
+++ JavaScript/Hoisting.js
@@ -0,0 +1,35 @@
+// console.log("Hello")
+// console.log("Hi")
+// console.log("How are you?")
+
+// console.log(x) //TDZ
+//TDZ
+let x = 10; //TDZ ends here
+console.log(x)
+
+
+
+// var x=10;
+// console.log(x)
+
+
+// greet()
+
+// function greet(){
+//     console.log("Helloo")
+//     console.log("How are you")
+// }
+
+// console.log(func)
+// var func = ()=>{
+//     console.log("Hello")
+// }
+
+// var fName = "Diksha";
+// function greet(fName){
+//     var greet = "Hello" + fName;
+//     return greet;
+// }
+// var greet1 = greet(fName);
+// var greet2 = greet("Nitish");
+// console.log(greet1, greet2)
