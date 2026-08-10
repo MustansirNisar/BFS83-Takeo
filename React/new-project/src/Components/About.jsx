1c3e700e422d242f3923ca5a66beb63b3e23f6c1
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-10 12:36:31 +0530

    Add files via upload

---
+++ React/new-project/src/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>About Component</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, unde.
+      </p>
+    </div>
+  )
+}
+
+export default About
