6907f78958b822b015ef9a6a01ef559788687c2d
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-11 10:23:22 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quos!</p>
+    </div>
+  )
+}
+
+export default Contact
