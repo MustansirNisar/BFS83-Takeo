014fb02f2d93ac2e06fd7977f6db193a24991f78
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-11 10:22:07 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>
+      <h1>Blog Component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, debitis!</p>
+    </div>
+  )
+}
+
+export default Blog
