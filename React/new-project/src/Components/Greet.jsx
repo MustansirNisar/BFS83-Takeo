94577ea69e20e53ffba160002a62d3291d36f2e0
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-14 10:20:21 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Greet.jsx
@@ -0,0 +1,14 @@
+
+function Greet(){
+    return(
+        <h1>This is another component: Greet</h1>
+    )
+}
+function Contact(){
+    return(
+        <h1>Contact me</h1>
+    )
+}
+export {Contact}
+
+export default Greet;
