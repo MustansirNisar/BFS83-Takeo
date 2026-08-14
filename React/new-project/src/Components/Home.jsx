c4b3a9440f2a79f87409434355bf60ee28c680f5
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-14 10:20:47 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, labore.</p>
+    </div>
+  )
+}
+
+export default Home
