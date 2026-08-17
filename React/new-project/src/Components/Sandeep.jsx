f6b63d486f30d35e9039d95b432cc19504afd8fa
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-08-17 10:05:42 +0530

    Add files via upload

---
+++ React/new-project/src/Components/Sandeep.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Sandeep() {
+  return (
+    <div>
+      <h1>Hello, my name is Sandeep</h1>
+      <p>I am a software Engineer</p>
+    </div>
+  )
+}
+
+export default Sandeep
